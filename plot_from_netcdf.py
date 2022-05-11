1dd1569c38b6acf80852aecb21603b6a3283315e
Félix Rodríguez Cardozo <frodriguezcardozo@gmail.com>
2022-05-11 18:02:03 -0400

    Update plot_from_netcdf.py

--- plot_from_netcdf.py
+++ plot_from_netcdf.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 import json
+import sys
 #My saviors today:
 #https://stackoverflow.com/questions/30509890/how-to-plot-a-smooth-2d-color-plot-for-z-fx-y
 #https://www.geeksforgeeks.org/read-json-file-using-python/
@@ -48,124 +49,125 @@ def plot3d(x,y,z,name,axis,inv_ax):
     plt.close()
     #plt.show()
 
-###Dip vs. Strike, best slip fixed
-def dip_strike(ds,indexes):
-    x_aux=ds['h'][:]
-    x=[]
-    for i in range(len(x_aux)):
-        rad = math.acos(x_aux[i])
-        x.append(math.degrees(rad))
-    x=np.array(x)
-    y=ds['kappa'][:]
-    z=[]
-    for i in range(len(x)):
-        for j in range(len(y)):
-            z.append(ds['__xarray_dataarray_variable__'][indexes[0]][indexes[1]][indexes[2]][i][indexes[4]][j][0])
+def h2d(h_values):
+    dip=[]
+    for i in range(len(h_values)):
+        rad = math.acos(h_values[i])
+        dip.append(math.degrees(rad))
+    dip=np.array(dip)
+    return(dip)
 
-    name='DipVsStrike_slip_fixed.pdf'
-    axis = ['Dip','Strike','Slip = {}'.format(ds['sigma'][indexes[4]])]
-    inv_ax = 'x'
-    plot3d(x,y,z,name,axis,inv_ax)
+def make_figure_plane(ds,indexes,x_axis,y_axis,free):
 
-####Slip vs. Strike, best dip fixed
-def slip_strike(ds,indexes):
-    x=ds['sigma'][:]
-    y=ds['kappa'][:]
-    z=[]
-    for i in range(len(x)):
-        for j in range(len(x)):
-            z.append(ds['__xarray_dataarray_variable__'][indexes[0]][indexes[1]][indexes[2]][i][j][indexes[5]][0])
+    fault_dict = {
+        'kappa': 'strike',
+        'sigma': 'slip',
+        'h': 'dip'
+    }
+    
+    print('Making slice plot for plane x_axis:{}, y_axis:{} and {} fixed '.format(fault_dict[x_axis],fault_dict[y_axis],fault_dict[free]))
 
-    axis = ['Slip','Strike','Dip = {}'.format(math.degrees(math.acos(ds['h'][indexes[5]])))]
-    name='SlipVsStrike_dip_fixed.pdf'
-    inv_ax = 'n'
-    plot3d(x,y,z,name,axis,inv_ax)
-
-####Slip vs. Dip, best strike fixed
-def slip_dip(ds,indexes):
-    x=ds['sigma'][:]
-    y_aux=ds['h'][:]
-    y=[]
-    for i in range(len(y_aux)):
-        rad = math.acos(y_aux[i])
-        y.append(math.degrees(rad))
-    y=np.array(y)
+    if x_axis == 'h':
+        x=h2d(ds[x_axis][:])
+    else:
+        x=ds[x_axis][:]
+    if y_axis == 'h':
+        y=h2d(ds[y_axis][:])
+    else:
+        y=ds[y_axis][:]
+    
     z=[]
 
     for i in range(len(x)):
         for j in range(len(y)):
-            z.append(ds['__xarray_dataarray_variable__'][indexes[0]][indexes[1]][indexes[2]][indexes[3]][j][i][0])
-    name='SlipVsDip_strike_fixed.pdf'
-    axis = ['Slip','Dip','Strike = {}'.format(ds['kappa'][indexes[3]])]
-    inv_ax = 'y'
+            if x_axis == 'h' and y_axis == 'kappa' and free == 'sigma':
+                #i=dip,j=strike,k=slip
+                z.append(ds['__xarray_dataarray_variable__'][indexes[0]][indexes[1]][indexes[2]][i][indexes[4]][j][0])
+            elif x_axis == 'sigma' and y_axis == 'kappa' and free == 'h':
+                #i=slip,j=strike,k=dip
+                z.append(ds['__xarray_dataarray_variable__'][indexes[0]][indexes[1]][indexes[2]][i][j][indexes[5]][0])
+            elif x_axis == 'sigma' and y_axis == 'h' and free == 'kappa':
+                #i=slip,j=dip,k=strike
+                z.append(ds['__xarray_dataarray_variable__'][indexes[0]][indexes[1]][indexes[2]][indexes[3]][j][i][0])
+                
+    if free == 'sigma':
+        free_value = ds[free][indexes[4]]
+    elif free == 'h':
+        free_value = ds[free][indexes[5]]
+        rad = math.acos(free_value)
+        free_value = math.degrees(rad)
+    elif free == 'kappa':
+        free_value = ds[free][indexes[3]]
+
+    name='{}_vs_{}_fixed_{}.pdf'.format(fault_dict[x_axis],fault_dict[y_axis],fault_dict[free])
+    axis = [fault_dict[x_axis],fault_dict[y_axis],'{} = {} '.format(fault_dict[free],free_value)]
+
+    if x_axis == 'h':
+        inv_ax = 'x'
+    elif y_axis == 'h':
+        inv_ax = 'y'
+    else:
+        inv_ax ='n'
+
     plot3d(x,y,z,name,axis,inv_ax)
 
-##############################
+def make_figure_surface(ds,indexes,x_axis,y_axis,free):
+    
+    fault_dict = {
+        'kappa': 'strike',
+        'sigma': 'slip',
+            'h': 'dip'
+    }
+    
+    print('Making surface plot for x_axis:{}, y_axis:{} and  {} free'.format(fault_dict[x_axis],fault_dict[y_axis],fault_dict[free]))
 
-###Dip vs. Strike, best slip at each coordinate
-def dip_strike_minmisfits(ds,indexes):
-    x_aux=ds['h'][:]
-    x=[]
-    for i in range(len(x_aux)):
-        rad = math.acos(x_aux[i])
-        x.append(math.degrees(rad))
-    x=np.array(x)
-    y=ds['kappa'][:]
+    if x_axis == 'h':
+        x=h2d(ds[x_axis][:])
+    else:
+        x=ds[x_axis][:]
+    if y_axis == 'h':
+        y=h2d(ds[y_axis][:])
+    else:
+        y=ds[y_axis][:]
+
     z=[]
-    for i in range(len(x)):
-        for j in range(len(y)):
-            z_aux = []
-            for k in range(len(ds['sigma'][:])):
-                z_aux.append(ds['__xarray_dataarray_variable__'][indexes[0]][indexes[1]][indexes[2]][i][k][j][0])
-            index_min = np.argmin(z_aux)
-            z.append(ds['__xarray_dataarray_variable__'][indexes[0]][indexes[1]][indexes[2]][i][index_min][j][0])
 
-    name='DipVsStrike_minimum_misfit.pdf'
-    axis = ['Dip','Strike','Minimun mistif at each dip-strike pair']
-    inv_ax = 'x'
-    plot3d(x,y,z,name,axis,inv_ax)
-
-####Slip vs. Strike, best dip at each coordinate
-def slip_strike_minmisfit(ds,indexes):
-    x=ds['sigma'][:]
-    y=ds['kappa'][:]
-    z=[]
-    for i in range(len(x)):
-        for j in range(len(x)):
-            z_aux = []
-            for k in range(len(ds['h'][:])):
-                z_aux.append(ds['__xarray_dataarray_variable__'][indexes[0]][indexes[1]][indexes[2]][i][j][k][0])
-            index_min = np.argmin(z_aux)
-            z.append(ds['__xarray_dataarray_variable__'][indexes[0]][indexes[1]][indexes[2]][i][j][index_min][0])
-
-    axis = ['Slip','Strike','Minimun mistif at each slip-strike pair']
-    name='SlipVsStrike_minimum_misfit.pdf'
-    inv_ax = 'n'
-    plot3d(x,y,z,name,axis,inv_ax)
-
-
-####Slip vs. Dip, best strike at each coordinate
-def slip_dip_minmisfits(ds,indexes):
-    x=ds['sigma'][:]
-    y_aux=ds['h'][:]
-    y=[]
-    for i in range(len(y_aux)):
-        rad = math.acos(y_aux[i])
-        y.append(math.degrees(rad))
-    y=np.array(y)
-    z=[]
     for i in range(len(x)):
         for j in range(len(y)):
             z_aux = []
-            for k in range(len(ds['kappa'][:])):
-                z_aux.append(ds['__xarray_dataarray_variable__'][indexes[0]][indexes[1]][indexes[2]][k][j][i][0])
+            for k in range(len(ds[free][:])):
+                if x_axis == 'h' and y_axis == 'kappa' and free == 'sigma':
+                    #i=dip,j=strike,k=slip
+                    z_aux.append(ds['__xarray_dataarray_variable__'][indexes[0]][indexes[1]][indexes[2]][i][k][j][0])
+                elif x_axis == 'sigma' and y_axis == 'kappa' and free == 'h':
+                    #i=slip,j=strike,k=dip
+                    z_aux.append(ds['__xarray_dataarray_variable__'][indexes[0]][indexes[1]][indexes[2]][i][j][k][0])
+                elif x_axis == 'sigma' and y_axis == 'h' and free == 'kappa':
+                    #i=slip,j=dip,k=strike
+                    z_aux.append(ds['__xarray_dataarray_variable__'][indexes[0]][indexes[1]][indexes[2]][k][j][i][0])
+                else:
+                    print('we have an issue with your axes {},{},{}'.format(x_axis,y_axis,free))
+                    sys.exit()
+                    
             index_min = np.argmin(z_aux)
-            z.append(ds['__xarray_dataarray_variable__'][indexes[0]][indexes[1]][indexes[2]][index_min][j][i][0])
-    name='SlipVsDip_strike_minimum_misfit.pdf'
-    axis = ['Slip','Dip','Minimun mistif at each slip-dip pair']
-    inv_ax = 'y'
-    plot3d(x,y,z,name,axis,inv_ax)
+            
+            if x_axis == 'h' and y_axis == 'kappa' and free == 'sigma':
+                z.append(ds['__xarray_dataarray_variable__'][indexes[0]][indexes[1]][indexes[2]][i][index_min][j][0])
+            elif x_axis == 'sigma' and y_axis == 'kappa' and free == 'h':
+                z.append(ds['__xarray_dataarray_variable__'][indexes[0]][indexes[1]][indexes[2]][i][j][index_min][0])
+            elif x_axis == 'sigma' and y_axis == 'h' and free == 'kappa':
+                z.append(ds['__xarray_dataarray_variable__'][indexes[0]][indexes[1]][indexes[2]][index_min][j][i][0])
 
+    axis = [fault_dict[x_axis],fault_dict[y_axis],'Minimun mistif at each {}-{} pair'.format(fault_dict[x_axis],fault_dict[y_axis])]
+    name='{}_vs_{}_minimum_misfit.pdf'.format(fault_dict[x_axis],fault_dict[y_axis])
+    if x_axis == 'h':
+        inv_ax = 'x'
+    elif y_axis == 'h':
+        inv_ax = 'y'
+    else:
+        inv_ax ='n'
+
+    plot3d(x,y,z,name,axis,inv_ax)
 
 f_netcdf = '20140826115645000DC_misfit.nc'
 f_json = '20140826115645000DC_solution.json'
@@ -173,13 +175,23 @@ ds = nc.Dataset(f_netcdf)
 G = read_json(f_json)
 indexes = find_index(ds,G)
 
+#sigma : rake/slip
+#kappa : strike
+#h: cos(dip)
+space=['sigma','kappa','h']
 
-##Planes:
-dip_strike(ds,indexes)
-slip_strike(ds,indexes)
-slip_dip(ds,indexes)
+##FIGURES PLANES
+##i=dip,j=strike,k=slip
+make_figure_plane(ds,indexes,space[2],space[1],space[0])
+##x=slip,y=strike,free=dip
+make_figure_plane(ds,indexes,space[0],space[1],space[2])
+##i=slip,j=dip,k=strike
+make_figure_plane(ds,indexes,space[0],space[2],space[1])
 
-##Surfaces:
-dip_strike_minmisfits(ds,indexes)
-slip_strike_minmisfit(ds,indexes)
-slip_dip_minmisfits(ds,indexes)
+##FIGURES SURFACES
+##i=dip,j=strike,k=slip
+make_figure_surface(ds,indexes,space[2],space[1],space[0])
+##x=slip,y=strike,free=dip
+make_figure_surface(ds,indexes,space[0],space[1],space[2])
+##i=slip,j=dip,k=strike
+make_figure_surface(ds,indexes,space[0],space[2],space[1])
