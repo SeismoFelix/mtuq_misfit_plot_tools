98ab1de60c5ace6084510ff925572bb741b957e0
Félix Rodríguez Cardozo <frodriguezcardozo@gmail.com>
2022-05-17 20:39:39 -0400

    Update misfit_double_couple.sh

--- GMT_map/misfit_double_couple.sh
+++ GMT_map/misfit_double_couple.sh
@@ -17,7 +17,7 @@
 #4.8471359451573206e-09
 #7.775461338251326e-09
 
-PSFILE=uncertainties3.ps
+PSFILE=uncertainties3b.ps
 COLOR1=color_misfit_dip_strike_surface_slip.txt
 COLOR2=color_misfit_slip_strike_surface_dip.txt
 COLOR3=color_misfit_slip_dip_surface_strike.txt
@@ -29,6 +29,6 @@ REGION2=-90/90/0/360
 REGION3=-90/90/0/90
 
 gmt psbasemap -R$REGION3 -JX10i/7i -Bx10.0 -Bya10.0 -BWeSn -K -V >> $PSFILE
-gmt psxy $COLOR3 -R -J -O -Wthin -Sc0.4 -V -Cmisfit3.cpt -K >> $PSFILE
-gmt psmeca $FOCMEC3 -G255/0/0 -R -T100 -J -Sa0.4 -C -h1 -O -V >> $PSFILE
+#gmt psxy $COLOR3 -R -J -O -Wthin -Sc0.4 -V -Cmisfit3.cpt -K >> $PSFILE
+gmt psmeca $FOCMEC3 -Zmisfit3.cpt -G255/0/0 -R -J -Sa0.4 -C -h1 -O -V >> $PSFILE
 gmt psconvert $PSFILE -Tf -P
